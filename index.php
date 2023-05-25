b7fac347d09f60a3db555374d1da093ee4cf870a
samu <samueljose.garcia@iesdonana.org>
2023-05-25 18:39:50 +0200

    Comprobaciones y variables de sesión.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
 
+    <?php
+        session_start();
+    ?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- main.php
+++ main.php
@@ -1,6 +1,11 @@
 <?php
 
-function esta_vacio($nombre, $ancho, $alto) {
+session_start();
+$_SESSION['almacen'] = array();
+
+
+function esta_vacio($nombre, $ancho, $alto)
+{
     if (isset($nombre, $ancho, $alto) && $nombre != "" && $ancho != "" && $alto != "") {
         return true;
     } else {
@@ -8,17 +13,23 @@ function esta_vacio($nombre, $ancho, $alto) {
     }
 }
 
-$almacen = array();
 
-if (esta_vacio($nombre = $_GET['nombre'], $ancho = $_GET['ancho'], $alto = $_GET['alto'])) { // Comprueba si están vacios y != null
+if (esta_vacio($_GET['nombre'], $_GET['ancho'], $_GET['alto'])) { // Comprueba si están vacios y != null
+    $nombre = $_GET['nombre'];
+    $ancho = (float) $_GET['ancho'];
+    $alto = (float) $_GET['alto'];
     if (is_numeric($ancho) && is_numeric($alto)) {
+        // Comprobar si la clave existe ya en el array.
+        if (array_key_exists($nombre, $_SESSION['almacen'])) {
+            echo "El producto ingresado ya existe";
+        } else {
+            $_SESSION['almacen'][$nombre] = array('ancho' => $ancho, 'alto' => $alto);
+            echo "Artículo agregado con éxito." . "\n";
 
-        var_dump($ancho);
-        var_dump($alto);
-        echo "zoi felí";
+            var_dump($_SESSION['almacen'][$nombre]);
+        }
     } else {
         echo "Los campos ancho y alto deben ser numéricos.";
-
     }
 } else {
     echo "Todos los campos deben contener algún dato.";
